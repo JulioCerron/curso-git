99d50ea8fa758afcf407cff76ce5b7980619fabc
JulioCerron <cerronyonathan@gmail.com>
2025-04-04 18:52:58 -0500

    suma

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,10 @@
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        int a = 10;
+        int b = 20;
+
+        int c = a + b;
+        System.out.println("el resultado es: " + c);
     }
 }
